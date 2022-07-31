4fbcea3f901e6b9a80f4d13131c32f5eed1c0da0
Yan Musikhin <yanmusikhin@gmail.com>
2022-07-31 19:36:13 +0400

    inti commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { GlobalStyle } from './style';
+import { Index } from './pages';
+
+export const App = () => {
+  return (
+    <>
+      <GlobalStyle/>
+      <Index/>
+    </>
+  );
+}
