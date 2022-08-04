ff389cca70865057fdf2720e49bec86afc2a960a
Yan Musikhin <yanmusikhin@gmail.com>
2022-08-04 21:40:17 +0400

    feat: add message when error

---
+++ src/components/error/error.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Title } from './style';
+
+export const Error = () => {
+  return (
+    <Container>
+      <Title>
+        Что-то пошло не так перезагрузите страницу
+      </Title>
+    </Container>
+  );
+};
---
+++ src/components/error/style.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+export const Title = styled.h3``;
