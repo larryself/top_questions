70bc4f7ac6ef79784e641ffb757d9c53cf20f940
Yan Musikhin <yanmusikhin@gmail.com>
2022-08-04 21:34:58 +0400

    fix: edit color

--- src/components/icon/approved/approved.tsx
+++ src/components/icon/approved/approved.tsx
@@ -2,7 +2,7 @@ import React, { FC, SVGProps } from 'react';
 import { COLORS } from '../../../constants/colors';
 
 export const Approved: FC<SVGProps<SVGSVGElement>> = ({color = COLORS.GREEN}) => (
-  <svg width={'36'} height={'36'} fill={'none'} xmlns={'http://www.w3.org/2000/svg'}>
+  <svg width={'36'} height={'36'} fill={color} xmlns={'http://www.w3.org/2000/svg'}>
     <path d="m6 14 8 8L30 6v8L14 30l-8-8v-8Z"></path>
   </svg>
 );
